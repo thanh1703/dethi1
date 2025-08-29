64ccc9a0e464cc8ebf929c66fa0cb9e8c58aed0a
thanh1703 <thanh.nh806@arena-multimedia.edu.vn>
2025-08-29 23:32:01 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+$('#a').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
